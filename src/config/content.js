4931ee65c7be4f984bc3a2cab9c3bf147953ac83
Mazharul haq <m220197@gmail.com>
2022-11-10 19:13:21 +0600

    age caltulation added

--- src/config/content.js
+++ src/config/content.js
@@ -1,3 +1,14 @@
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
 const content = {
   autoTypingStrings: [
     "a Machine learning enthusiast",
@@ -5,7 +16,7 @@ const content = {
     "a Freelancer",
   ],
   name: "Mozharul Haq",
-  age: 25,
+  age: getAge("1997-1-22"),
   aboutText:
     "Self-motivated and passionate programmer with a Computer Science and Engineering bachelor's degree. I have worked on various projects using React, Next.js, Python, Flutter, C#. I taught my self the basics of Machine Learning and Data Science. I am currently working as a Freelancer. I am always looking for new opportunities to learn and grow.",
   phoneNumber: "+8801765393016",
